4a20aa4d16db5bf6b9107c5febea92a47371fbc1
Éverton Machado Rosa <everton.mr1@gmail.com>
2021-03-07 20:37:11 -0300

    Resetflix com H2

--- Aula05/RESETFLIX-H2-main/src/main/java/br/com/cwi/resetflix/repository/DiretoresRepository.java
+++ Aula05/RESETFLIX-H2-main/src/main/java/br/com/cwi/resetflix/repository/DiretoresRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DiretorRepository extends JpaRepository<DiretorEntity, Long> {
+public interface DiretoresRepository extends JpaRepository<DiretorEntity, Long> {
 
 }
---
+++ Aula05/RESETFLIX-H2-main/src/main/java/br/com/cwi/resetflix/repository/FilmeRepository.java
@@ -0,0 +1,14 @@
+package br.com.cwi.resetflix.repository;
+
+import br.com.cwi.resetflix.domain.Genero;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.cwi.resetflix.entity.FilmeEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FilmeRepository extends JpaRepository<FilmeEntity, Long> {
+   List<FilmeEntity> findAllByGenero(Genero genero);
+}
